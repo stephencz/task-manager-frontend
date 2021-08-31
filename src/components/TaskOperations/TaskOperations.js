5d4852c13f298ec841d6f8e56f05eb3ae38b4b69
Stephen Czekalski <admin@stephencz.com>
2021-08-31 13:13:59 -0400

    Task Operations

--- src/components/TaskOperations/TaskOperations.js
+++ src/components/TaskOperations/TaskOperations.js
@@ -5,7 +5,15 @@ const TaskOperations = (props) => {
 
   return (
     <div className="task-operations">
-
+      <input className="search" placeholder="Search"></input>
+      <select>
+        <option value="">Default</option>
+        <option value="">Sort by Description</option>
+        <option value="">Sort by Date</option>
+        <option value="">Sort by Tag</option>
+      </select>
+      <button>Add Task</button>
+      <button>Add Tag</button>
     </div>
   )
 
